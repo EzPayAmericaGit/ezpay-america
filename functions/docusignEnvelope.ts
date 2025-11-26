07470220df7bb0f240370cff38f7b24685d30746
Stmeo Nona <spudedu@gmail.com>
2025-11-26 20:25:27 +0000

    File changes

--- functions/docusignEnvelope.ts
+++ functions/docusignEnvelope.ts
@@ -1,5 +1,6 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.4';
-import * as jose from 'npm:jose@5.2.0';
+import { SignJWT } from 'npm:jose@5.2.0';
+import { createPrivateKey } from 'node:crypto';
 
 const DOCUSIGN_AUTH_SERVER = 'account.docusign.com'; // Production
 const DOCUSIGN_BASE_URL = 'https://na4.docusign.net/restapi'; // Production
