afee9bf9ce43d29c0655a6a3b65eee99a3fd1e81
Stmeo Nona <spudedu@gmail.com>
2025-12-08 21:07:53 +0000

    File changes

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -74,7 +74,7 @@ export default function HeroSection() {
             initial={{ opacity: 0, scale: 0.9 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.8, delay: 0.2 }}
-            className="relative"
+            className="relative scale-[2] origin-center"
           >
             <div className="relative rounded-3xl overflow-hidden shadow-2xl">
               <img
