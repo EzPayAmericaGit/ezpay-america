91e80de0269f1a4caa8437c2d6f957aced230972
Stmeo Nona <spudedu@gmail.com>
2025-12-09 16:38:32 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,7 +4,7 @@ import { createPageUrl } from "@/utils";
 import { Phone, Menu, X, ChevronDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { base44 } from "@/api/base44Client";
-import AnalyticsTracker from "../components/AnalyticsTracker";
+import AnalyticsTracker from "@/components/AnalyticsTracker";
 
 export default function Layout({ children }) {
   const location = useLocation();
