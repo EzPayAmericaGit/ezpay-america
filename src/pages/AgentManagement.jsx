5264614f10f9ab3b3af3dda102e76ce719aac09c
Stmeo Nona <spudedu@gmail.com>
2026-02-07 18:28:57 +0000

    File changes

--- src/pages/AgentManagement.jsx
+++ src/pages/AgentManagement.jsx
@@ -285,6 +285,7 @@ export default function AgentManagement() {
               </div>
             </DialogContent>
           </Dialog>
+          </div>
         </div>
 
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
